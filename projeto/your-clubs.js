2f7a172f85a2c93cb09c5bb94a5e09ff8b7f897f
EllieFranca <franca.ems@gmail.com>
2025-05-30 12:42:28 +0000

    ainda tentando resolver o problema do function (eu vou coringaaaaaarrr)

--- projeto/your-clubs.js
+++ projeto/your-clubs.js
@@ -1,7 +1,21 @@
-const input = document.getElementsById('searchbar');
-        
-        input.addEventListener('keydown', function(event) {
-            if (event.keyCode === 'enter') {
-                console.log('enter pressionado')
-            }
-        })
+// let pesquisa = document.getElementById('searchbar_input');
+
+// function buscar(pesquisa) {
+//     return pesquisa;
+//     console.log(pesquisa)
+// }
+
+
+// document.getElementById('lupe_icon').addEventListener('click', function(buscar){})
+
+// let pesquisa = document.getElementById('searchbar_input')
+    
+// function buscar(pesquisa) {
+//         return pesquisa;
+//         console.log(pesquisa)
+//     }
+function buscar(event){
+    event.preventDefault();
+
+    let pesquisa = document.getElementById('searchbar_input');
+}
