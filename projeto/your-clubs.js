edc528dec96d045cfed386a40c6ddf2063a3a4a2
EllieFranca <franca.ems@gmail.com>
2025-05-31 20:33:44 +0000

    .

--- projeto/your-clubs.js
+++ projeto/your-clubs.js
@@ -8,6 +8,7 @@ function buscar() {
     if(livros == ''){ //Caso não haja nada na barra de pesquisa, a mensagem parece, 
             document.getElementById('sem_pesquisa').classList.remove('div_oculta')
             document.getElementById('sem_clubes').classList.add('div_oculta')
+            document.getElementById('pesquisa_clubs').classList.add('div_oculta');
 
     } else {
         mostrar_lista_pesquisa()
@@ -24,6 +25,7 @@ function buscar() {
             data.docs.slice(0,7).forEach((e) =>{
                 console.log(e.title)
                 console.log(e.author_name)
+                
             }) //pesquisa do json atributos específicos utilizados, com o slice limitando a pesquisa a 5 elementos
 
         })
@@ -34,6 +36,7 @@ function buscar() {
 }
 
 function mostrar_lista_pesquisa() {
+    document.getElementById('sem_pesquisa').classList.add('div_oculta')
     document.getElementById('sem_clubes').classList.add('div_oculta')
     document.getElementById('pesquisa_clubs').classList.remove('div_oculta');
 }
